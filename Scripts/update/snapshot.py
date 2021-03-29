59cbb61a355fb12f785e1acd0b726002e6c09a8a
Sntx626 <langer.leonard@gmail.com>
2021-03-30 00:24:48 +0200

    Improved snapshot name formatting

--- Scripts/update/snapshot.py
+++ Scripts/update/snapshot.py
@@ -8,7 +8,7 @@ def finalize() -> None:
         print("No snapshot in data.json present!")
         return
     most_recent_snapshot_name = data["managed_snapshot"]
-    os.system(f"echo finalized snapshot {most_recent_snapshot_name}")
+    os.system(f"sudo lvremove /dev/volgroup0/{most_recent_snapshot_name}")
     data["managed_snapshot"] = None
     print("Finalized snapshot:", most_recent_snapshot_name)
     json.dump(data, open("/home/leo/Scripts/update/data.json", "w"), indent=2)
@@ -19,7 +19,7 @@ def rollback() -> None:
         print("No snapshot in data.json present!")
         return
     most_recent_snapshot_name = data["managed_snapshot"]
-    os.system(f"echo tolling back to this snapshot {most_recent_snapshot_name}")
+    os.system(f"lvconvert --merge /dev/volgroup0/{most_recent_snapshot_name}")
     data["managed_snapshot"] = None
     print("Rolled back to snapshot:", most_recent_snapshot_name)
     json.dump(data, open("/home/leo/Scripts/update/data.json", "w"), indent=2)
@@ -30,8 +30,8 @@ def take_snapshot() -> None:
         print("Snapshot in data.json present!")
         return
     now = datetime.datetime.now()
-    most_recent_snapshot_name = f"root_snapshot_{now.year}-{now.month}-{now.day}_{now.hour}{now.minute}"
-    os.system(f"echo taking snapshot {most_recent_snapshot_name}")
+    most_recent_snapshot_name = f"root_snapshot_{now.year}-{now.month}-{now.day}_{str(now.hour).zfill(2)}{str(now.minute).zfill(2)}"
+    os.system(f"sudo lvcreate -L 32GB -s -n {most_recent_snapshot_name} /dev/mapper/volgroup0-lv_root")
     data["managed_snapshot"] = most_recent_snapshot_name
     print("Took snapshot:", most_recent_snapshot_name)
     json.dump(data, open("/home/leo/Scripts/update/data.json", "w"), indent=2)
